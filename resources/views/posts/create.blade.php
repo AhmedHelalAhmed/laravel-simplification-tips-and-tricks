11f8927caa61923d91c67442d4006688d2be2ad6
Ahmed Helal Ahmed <ahmed.helal.online@gmail.com>
2020-03-21 01:19:22 +0200

    add back button

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -4,6 +4,7 @@
         <form method="post" action="{{ route('posts.store') }}">
             @method('post')
             @include('posts.form')
+            <a class="btn btn-info" href="{{ route('posts.index') }}">Back to all posts</a>
         </form>
     </div>
 @endsection
--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -5,6 +5,7 @@
         <form method="post" action="{{ route('posts.update',$post->id) }}">
             @method('put')
             @include('posts.form')
+            <a class="btn btn-info" href="{{ route('posts.index') }}">Back to all posts</a>
         </form>
     </div>
 @endsection
