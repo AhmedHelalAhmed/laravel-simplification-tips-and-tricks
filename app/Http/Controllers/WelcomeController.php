d600ad177f7d7df4777b865cf8632ec33e366940
Ahmed Helal Ahmed <ahmed.helal.online@gmail.com>
2020-03-20 16:45:53 +0200

    fix mistake in spelling

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 class WelcomeController
 {
     // if you need and trait from Controller
-    // you casn use this here like
+    // you can use this here like
     // use AuthorizesRequests;
     // and use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
     public function __invoke()
